5aee8df5f644424e0c0ccf81c9a16426d96ebf83
Master <1204332316@qq.com>
2023-06-08 16:47:57 +0800

    小更改

--- Chapter2/errorcode-demo/src/test/java/geektime/spring/data/errorcodedemo/ErrorCodeDemoApplicationTests.java
+++ Chapter2/errorcode-demo/src/test/java/geektime/spring/data/errorcodedemo/ErrorCodeDemoApplicationTests.java
@@ -5,6 +5,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
@@ -14,6 +15,7 @@ public class ErrorCodeDemoApplicationTests {
 	private JdbcTemplate jdbcTemplate;
 
 	@Test(expected = CustomDuplicatedKeyException.class)
+	@Sql(scripts = "classpath:schema.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
 	public void testThrowingCustomException() {
 		jdbcTemplate.execute("INSERT INTO FOO (ID, BAR) VALUES (1, 'a')");
 		jdbcTemplate.execute("INSERT INTO FOO (ID, BAR) VALUES (1, 'b')");
